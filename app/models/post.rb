50bef700ee848658066eb0df86964aab97bbfe84
Zuadinho <alancostacs@hotmail.com>
2024-09-05 10:13:06 -0300

    Views e Rotas

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,7 @@
 class Post < ApplicationRecord
   validates :title, :description, presence: true
-  validates :title, length: { minimum: 5 }
-  validates :description, length: { minimum: 10 }
+  validates :title, length: { minimum: 3 }
+  validates :description, length: { minimum: 3 }
 
-  before_save do
-    self.title = self.title + " Alan"
-  end
+  
 end
